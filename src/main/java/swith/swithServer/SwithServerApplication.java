582f3c38eee80ce21b2f5c309c141c3b371ed4f5
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-10-15 13:04:17 +0900

    프로젝트 파일 업로드

---
+++ src/main/java/swith/swithServer/SwithServerApplication.java
@@ -0,0 +1,13 @@
+package swith.swithServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwithServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwithServerApplication.class, args);
+	}
+
+}
