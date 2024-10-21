18ef796292224bb1e6dfb9d0d5ad56dd8d768117
Soohyun Park <pshyun03@gmail.com>
2024-10-21 15:36:07 +0900

    study에 extends 추가

--- src/main/java/swith/swithServer/domain/Study/Entity/Study.java
+++ src/main/java/swith/swithServer/domain/Study/Entity/Study.java
@@ -4,13 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import swith.swithServer.domain.Group.Entity.Group;
+import swith.swithServer.domain.common.BaseEntity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
 @Entity
-public class Study {
+public class Study extends BaseEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long study_id;
