e3e85bad920f17df2597ec9eaea943a55192c082
JuHyeonAh <jha08130930@gmail.com>
2024-11-23 23:53:34 +0900

    user_task_API complete

--- src/main/java/swith/swithServer/domain/usertask/service/UserTaskService.java
+++ src/main/java/swith/swithServer/domain/usertask/service/UserTaskService.java
@@ -39,27 +39,3 @@ public class UserTaskService {
         return newStatus.name();
     }
 }
-//    @Transactional
-//    public String updateTaskStatus(Long userId, Long taskId, String taskStatus) {
-//        TaskStatus newStatus;
-//        try {
-//            newStatus = TaskStatus.valueOf(taskStatus.toUpperCase());
-//        } catch (IllegalArgumentException e) {
-//            throw new BusinessException(ErrorCode.NOT_VALID_ERROR);
-//        }
-//
-//        UserTask userTask = userTaskRepository.findByUserIdAndId(userId, taskId)
-//                .orElseThrow(() -> new BusinessException(ErrorCode.USER_TASK_NOT_FOUND));
-//
-//        if (newStatus == TaskStatus.COMPLETED && userTask.getTaskStatus() == TaskStatus.COMPLETED) {
-//            throw new BusinessException(ErrorCode.TASK_ALREADY_COMPLETED);
-//        }
-//
-//        if (newStatus == TaskStatus.PENDING && userTask.getTaskStatus() != TaskStatus.COMPLETED) {
-//            throw new BusinessException(ErrorCode.TASK_NOT_COMPLETED);
-//        }
-//
-//        userTask.updateTaskStatus(newStatus);
-//        return newStatus.name();
-//    }
-// }
