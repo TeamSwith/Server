6124ccb445542994136592f3fd3cb76e4cca3ea3
Soohyun Park <pshyun03@gmail.com>
2024-12-03 12:27:26 +0900

    feat(#81) 출석 생성, 출석 상태 변경 구현

---
+++ src/main/java/swith/swithServer/domain/attend/controller/AttendController.java
@@ -0,0 +1,39 @@
+package swith.swithServer.domain.attend.controller;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import swith.swithServer.domain.attend.dto.AttendResponse;
+import swith.swithServer.domain.attend.entity.Attend;
+import swith.swithServer.domain.attend.service.AttendService;
+import swith.swithServer.domain.studyGroup.entity.StudyGroup;
+import swith.swithServer.domain.studyGroup.service.GroupService;
+import swith.swithServer.domain.task.entity.Task;
+import swith.swithServer.domain.task.service.TaskService;
+import swith.swithServer.domain.user.entity.User;
+import swith.swithServer.global.oauth.service.OauthService;
+import swith.swithServer.global.response.ApiResponse;
+
+@RestController
+@RequestMapping("/group/{id}/study/{studyId}")
+@RequiredArgsConstructor
+@Tag(name="출석")
+public class AttendController {
+    private final OauthService authService;
+    private final GroupService groupService;
+    private final AttendService attendService;
+
+    @PatchMapping("/attend")
+    @Operation(summary = "출석 상태 변경")
+    public ApiResponse<AttendResponse> updateAttend(
+            @PathVariable Long id,
+            @PathVariable Long studyId
+    ){
+        StudyGroup studyGroup = groupService.getGroupById(id);
+        User user = authService.getLoginUser();
+        Attend updatedAttend = attendService.updateAttend(user, studyId);
+        return new ApiResponse<>(200, AttendResponse.from(updatedAttend));
+    }
+}
---
+++ src/main/java/swith/swithServer/domain/attend/repository/AttendRepository.java
@@ -0,0 +1,12 @@
+package swith.swithServer.domain.attend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import swith.swithServer.domain.attend.entity.Attend;
+import swith.swithServer.domain.study.entity.Study;
+import swith.swithServer.domain.user.entity.User;
+
+import java.util.Optional;
+
+public interface AttendRepository extends JpaRepository<Attend, Long> {
+    Optional<Attend> findByUserAndStudy(User user, Study study);
+}
