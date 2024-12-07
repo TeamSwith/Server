e727594714e303f5899cd97d02a3a377eab85ccc
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-12-07 15:02:15 +0900

    수정

--- src/main/java/swith/swithServer/domain/sse/controller/SseController.java
+++ src/main/java/swith/swithServer/domain/sse/controller/SseController.java
@@ -22,30 +22,12 @@ public class SseController {
     public ResponseEntity<SseEmitter> connect() {
         Long userId = authService.getLoginUser().getId(); // 로그인한 사용자의 ID 가져오기
         SseEmitter sseEmitter = sseEmitters.add(userId);
-
-        sseEmitter.onCompletion(() -> { // 연결이 정상적으로 완료된 경우
-            System.out.println("SseEmitter completed");
-            sseEmitters.cleanupEmitter(userId);
-        });
-
-
-        sseEmitter.onTimeout(() -> { // 연결 타임아웃 발생 시
-            System.out.println("SseEmitter timed out");
-            sseEmitters.cleanupEmitter(userId);
-        });
-
-
-        sseEmitter.onError((ex) -> { // 연결 중 오류 발생 시
-            System.out.println("SseEmitter error: " + ex.getMessage());
-            sseEmitters.cleanupEmitter(userId);
-        });
-
         return ResponseEntity.ok(sseEmitter);
     }
 
     @DeleteMapping("/cleanUserEmitter") // 특정 회원의 emitter 삭제
     public ResponseEntity<String> clean(){
-        Long userId = authService.getLoginUser().getId(); // 로그인한 사용자의 ID 가져오기
+        Long userId = authService.getLoginUser().getId();
         return ResponseEntity.ok(sseEmitters.cleanupEmitter(userId));
     }
 
